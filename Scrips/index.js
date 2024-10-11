010d8b0f232f47b86ad53b2113f1f1762ec0cb5e
Angela <angela.rodriguez@davinci.edu.ar>
2024-10-11 14:03:59 -0300

    function

--- Scrips/index.js
+++ Scrips/index.js
@@ -1,8 +1,23 @@
 "use strict"
 
-const nombredediscos = ["Please Please Me", "With the Beatles", "A Hard Day's Night", "Beatles for Sale"];
-console.log(nombredediscos);
 
+/*Validaciones
+
+El nombre del disco, autor/banda y nombre de la pista no pueden quedar vacíos.
+El código numérico único del disco no puede ser menor a 1, ni mayor a 999.
+La duración de cada pista debe estar entre 0 y 7200 (segundos) inclusive.
+
+*/
+
+/*
+const nombredediscos = ["Please Please Me"; "With the Beatles"; "A Hard Day's Night"; "Beatles for Sale"];
+*/
+
+function nombredediscos(autor, nombrepista,duración){
+    this.autor = autor;
+    this.nombrepista = nombrepista;
+    this.duracion = duración;
+}
 
 
 
