dd641fa9dc502290536356c390c96dba62b17a1c
Angela <angela.rodriguez@davinci.edu.ar>
2024-10-10 22:42:54 -0300

    se agrega index.js

---
+++ Scrips/index.js
@@ -0,0 +1,25 @@
+"use strict"
+
+const nombredediscos = ["Please Please Me", "With the Beatles", "A Hard Day's Night", "Beatles for Sale"];
+console.log(nombredediscos);
+
+
+
+
+
+/*
+Portada 
+Codigo numerico disco menor a 1, ni mayor a 999.
+Pistas nombre
+Duración 0 y 7200 (segundos)
+*/
+
+function cargarDisco () {
+    nombredediscos = prompt("Como se llama el disco");
+    console.log (disco1);
+}
+/*
+function mostrarDisco() {
+    console.log("El nombre del disco es $(nombredeldisco)";
+}
+    */
