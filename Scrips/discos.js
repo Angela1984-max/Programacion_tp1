462daded33b3f6e3991df57a49cac709e7dd60fd
Angela <angela.rodriguez@davinci.edu.ar>
2024-10-18 22:54:16 -0300

    class disco

---
+++ Scrips/discos.js
@@ -0,0 +1,9 @@
+class disco {
+    constructor(nombre, pista, duración, autor, portada){
+        this.nombre = nombre;
+        this.pista = pista;
+        this.duración = duración;
+        this.autor = autor;
+        this.portada = portada;
+    }
+}
